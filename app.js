d45d276564292832cb81281bdd149f45841bedb2
Alonso Acevedo <uacevedo22@hotmail.com>
2025-01-29 14:40:05 -0500

    Funcion visualizarLista

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let listaDeAmigos = [];
 
+//Asigna texto (elemento HTML, Texto a)
 function asignarTextoElemento (elemento,texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
@@ -18,3 +19,11 @@ function ingresarNombre (){
     }
 }
 
+function visualizarLista (){
+    let listaHTML = document.querySelector("ul");
+    listaHTML.innerHTML = "";
+    for ( i = 0; i < listaDeAmigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = listaDeAmigos[i];
+    }
+}
